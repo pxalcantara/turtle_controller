86567ad50dfcffa834353e71937b72fe60a6e8d0
Pedro Alcantara <pxalcantara@gmail.com>
2024-07-17 19:48:08 -0300

    move keeping only with enter key

--- src/controller.cpp
+++ src/controller.cpp
@@ -16,8 +16,8 @@ class Controller : public rclcpp::Node
 {
 public:
     Controller() : Node("move_controller") {
-        publisher = this->create_publisher<std_msgs::msg::String>("topic", 10);
-        cmd_vel_pub = this->create_publisher<geometry_msgs::msg::Twist>("/turtle1/cmd_vel", 10);    
+        publisher = this->create_publisher<std_msgs::msg::String>("commands", 10);
+        cmd_vel_pub = this->create_publisher<geometry_msgs::msg::Twist>("/cmd_vel", 10);    
         RCLCPP_INFO(this->get_logger(), "controller criado");
 
         command_sub = this->create_subscription<keyboard_msgs::msg::Key>("keyup", 10, std::bind(&Controller::commandCallback, this, _1));
@@ -41,12 +41,33 @@ public:
         cmd_position_reference = 0;
         move = false;
         MOVING_THRESHOLD = 0.01;
-        distance = 0.0;
+        current_distance = 0.0;
 
     };
 
     double calculate_distance(double initial_x, double initial_y, double final_x, double final_y) {
         return std::sqrt(std::pow(final_x - initial_x, 2) + std::pow(final_y - initial_y, 2));
+    }
+
+    void stop() {
+        cmd_vel_msg.linear.x = 0.0;
+        cmd_vel_msg.angular.z = 0.0;
+        this->cmd_vel_pub->publish(cmd_vel_msg);
+    }
+
+    void move_forward() {
+        // cmd_vel_msg.linear.x = 0.2;
+        // cmd_vel_msg.angular.z = 0.0;
+        // this->cmd_vel_pub->publish(cmd_vel_msg);
+        move_forward(0.2);
+    }
+
+    void move_forward(double speed) {
+        move = true;
+        // start_position = current_distance;
+        cmd_vel_msg.linear.x = speed;
+        cmd_vel_msg.angular.z = 0.0;
+        this->cmd_vel_pub->publish(cmd_vel_msg);
     }
 
 private:
@@ -64,14 +85,16 @@ private:
     int cmd_position_reference;
     bool move;
     float MOVING_THRESHOLD;
-    double distance;
+    double current_distance;
+    double start_position;
     
 
 
     void commandCallback(const keyboard_msgs::msg::Key::SharedPtr msg) {
-        // RCLCPP_INFO(this->get_logger(), std::to_string(msg->code).c_str());
+        RCLCPP_INFO(this->get_logger(), std::to_string(msg->code).c_str());
         if (msg->code == 13) {
             move = true;
+            start_position = current_distance;
             return;
         }
         
@@ -80,19 +103,32 @@ private:
     }
 
     void poseCallback(const nav_msgs::msg::Odometry::SharedPtr msg) {
-        if ( std::abs(msg->twist.twist.linear.x) > MOVING_THRESHOLD || std::abs(msg->twist.twist.angular.z) > MOVING_THRESHOLD ) {
-            move = true;
-        } else {
-            move = false;
-        }
+        // if ( std::abs(msg->twist.twist.linear.x) > MOVING_THRESHOLD || std::abs(msg->twist.twist.angular.z) > MOVING_THRESHOLD || move) {
+        //     if (!move) {
+        //         start_position = current_distance;
+        //     }
+        //     move = true;
+
+        // } else {
+        //     move = false;
+        // }
         
         if (move) {
-            distance += calculate_distance(initial_pose.position.x, initial_pose.position.y, msg->pose.pose.position.x, msg->pose.pose.position.y);
-            // initial_pose.position.x = msg->pose.pose.position.x;
-            // initial_pose.position.y = msg->pose.pose.position.y;
+            double moving_distance = std::abs(current_distance - start_position);
+            if (msg->twist.twist.linear.x < 0) {
+                move_forward(0.2);
+            } else {
+                move_forward(-0.1);
+            }
+
+            if (moving_distance > 1) {
+                stop();
+                return;
+            }
+            current_distance += calculate_distance(initial_pose.position.x, initial_pose.position.y, msg->pose.pose.position.x, msg->pose.pose.position.y);
             initial_pose = msg->pose.pose;
+            RCLCPP_INFO_STREAM(this->get_logger(), "Distancia:" << current_distance << " delta: " << moving_distance << " init:" << start_position);
         }
-        RCLCPP_INFO_STREAM(this->get_logger(), "Distancia:" << distance << " moving" << move);
         // if(msg->angular_velocity != 0 || msg->linear_velocity != 0) {
         //     if(cmd_position_reference == commands.size()) {
         //         commands.clear();
@@ -102,11 +138,11 @@ private:
 
         //     return;
         // }
-        // if(!commands.empty() && move) {
-        //     // RCLCPP_INFO(this->get_logger(), std::to_string(commands[cmd_position_reference]).c_str());
-        //     publishCommand(commands[cmd_position_reference]);
-        //     cmd_position_reference++;
-        // }
+        if(!commands.empty() && move) {
+            // RCLCPP_INFO(this->get_logger(), std::to_string(commands[cmd_position_reference]).c_str());
+            publishCommand(commands[cmd_position_reference]);
+            cmd_position_reference++;
+        }
     }
 
     void publishCommand(const uint16_t &command){
