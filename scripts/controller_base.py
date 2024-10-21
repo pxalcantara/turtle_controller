79302dd68296163799b64b50108c7b9b8c07a326
Pedro Alcantara <pxalcantara@gmail.com>
2024-10-21 16:41:33 -0300

    update and clean package

--- scripts/controller_base.py
+++ scripts/controller_base.py
@@ -8,36 +8,17 @@ from std_msgs.msg import Bool
 from turtle_controller.msg import RobotStatus
 from turtle_controller.msg import RobotCmd
 
-@dataclass
-class Commands:
-    direction: str
-    velocity: float
-    limit: float    
-
-steps = [
-    Commands(direction='front', velocity=0.5, limit=1.0),
-    Commands(direction='left', velocity=0.5, limit=1.57),
-    Commands(direction='front', velocity=0.5, limit=1.0),
-]
-
-
 class ControllerClient(Node):
     def __init__(self):
         super().__init__("controller_client")
         self.status_sub = self.create_subscription(RobotStatus, '/robot_status', self.status_callback, 10)
-        self.start_sub = self.create_subscription(Bool, 'client_start', self.start_callback, 10)
         self.cmd_pub = self.create_publisher(RobotCmd, 'robot_cmd', 10)
 
-        self.start_flag = False
         self.moving = False
         self.cmd_msg = RobotCmd()
         self.get_logger().info("Client sensor!")
         self.cmd_count = 0
-    
-    def start_callback(self, msg):
-        self.start_flag = msg.data
-        self.get_logger().info(f"Iniciando {msg.data}")
-        self.cmd_count = 0
+
     
     def check_moving(self, msg):
         if (msg.moving == True):
@@ -60,14 +41,12 @@ class ControllerClient(Node):
         self.cmd_pub.publish(self.cmd_msg)
 
     def status_callback(self, msg):
-        if(not(self.start_flag)):
-            return
-
+        # Escreva o código nessa funcao, lembre-se, ela é executada em loop
+        # Faz com que os comandos seguintes sejam ignorados se o robo estiver se movendo
         if (self.check_moving(msg)):
             return
         
-# ======== Escrever o código de funcionamento aqui ==================== 
-            
+
 
 def main(args=None):
     rclpy.init(args=args)
